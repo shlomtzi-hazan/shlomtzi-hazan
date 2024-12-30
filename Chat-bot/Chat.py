a2f4634c9c6e73d09795862e57aac7a9c3ef617d
shlomtzi-hazan <shlomtz.ev@gmail.com>
2024-12-30 11:17:07 -0500

    Update Chat.py
    Add .env file

--- Chat-bot/Chat.py
+++ Chat-bot/Chat.py
@@ -1,15 +1,26 @@
+from dotenv import load_dotenv
+import os
 from langchain_openai import ChatOpenAI
 from langchain.agents import initialize_agent, AgentType
 from langchain.agents import load_tools
 from langchain.tools import Tool
 
-tools = load_tools(["searx-search"],
-                    searx_host="https://eladsearngx-w4o7wofqcq-nn.a.run.app/",
-                    engines=["google"])
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve variables from the environment
+api_key = os.getenv("OPENAI_API_KEY")
+searx_host = os.getenv("SEARX_HOST")
+searx_engines = os.getenv("SEARX_ENGINES").split(",")
+model = os.getenv("OPENAI_MODEL")
+
+tools = load_tools(
+    ["searx-search"],
+    searx_host=searx_host,
+    engines=searx_engines
+)
 
 def conversetional_input(user_input):
-    """Fallback tool: This tool provides conversational responses directly from the LLM for user queries that do not require real-time information or online search, such as general knowledge or static information already contained within the model."""
-    # Criterion for invoking this tool
     print(f"Searching for: {user_input}")
     return llm.invoke(user_input)
 
@@ -21,14 +32,10 @@ tools.append(
     )
 )
 
-with open("/Users/shlomtzi/PycharmProjects/api_key.txt", "r") as file:
-    api_key = file.read().strip()
-
-llm = ChatOpenAI(api_key=api_key, model="gpt-4-turbo")
+llm = ChatOpenAI(api_key=api_key, model=model)
 agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
 
 def chat():
-    # Start the conversation
     print("Chat started! Type 'exit' to end the conversation.")
 
     while True:
@@ -36,26 +43,19 @@ def chat():
         if user_input.lower() == "exit":
             print("Ending chat.")
             break
-        
-        # Get the response from the agent
+
         response = agent.invoke(user_input)
-       
         try:
-            # Check if response is a dictionary and print the keys
             if isinstance(response, dict):
-                # Access the 'output' key to get the model's response
                 if 'output' in response:
                     clean_response = response['output'].replace("\n", " ").strip()
                 else:
-                    clean_response = str(response)  # Fallback if 'output' key is missing
+                    clean_response = str(response)
             else:
-                # If the response is a string, clean it directly
                 clean_response = response.replace("\n", " ").strip()
-            
             print(f"OpenAI: {clean_response.strip()}")
         except Exception as e:
             print(f"Error: {e}")
-
 
 if __name__ == "__main__":
     chat()
